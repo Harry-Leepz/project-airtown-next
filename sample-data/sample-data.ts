62ed24f30640b74af1449af5b0ac477c09561b51
Harry Dhillon <hdhillon478@gmail.com>
2025-01-22 19:35:55 +0000

    feat: add sample data for product listings

---
+++ sample-data/sample-data.ts
@@ -0,0 +1,60 @@
+const sampleData = {
+  products: [
+    {
+      name: "Air Jordan 1 Mid",
+      slug: "Air Jordan 1 Mid",
+      category: "Men's Shoes",
+      description:
+        "Inspired by the original AJ1, this mid-top edition maintains the iconic look you love while choice colours and crisp leather give it a distinct identity.",
+      images: [
+        "/images/sample-products/a-one.png",
+        "/images/sample-products/a-two.png",
+      ],
+      price: 90.99,
+      brand: "Jordan",
+      rating: 4.8,
+      numReviews: 10,
+      stock: 5,
+      isFeatured: true,
+      banner: "banner-one.png",
+    },
+    {
+      name: "Air Jordan 1 Low",
+      slug: "Air Jordan 1 Low",
+      category: "Men's Shoes",
+      description:
+        "Inspired by the original AJ1, this mid-top edition maintains the iconic look you love while choice colours and crisp leather give it a distinct identity.",
+      images: [
+        "/images/sample-products/a-one.png",
+        "/images/sample-products/a-two.png",
+      ],
+      price: 90.99,
+      brand: "Jordan",
+      rating: 4.8,
+      numReviews: 10,
+      stock: 5,
+      isFeatured: true,
+      banner: "banner-one.png",
+    },
+    {
+      name: "Air Jordan 1 High",
+      slug: "Air Jordan 1 High",
+      category: "Men's Shoes",
+      description:
+        "Inspired by the original AJ1, this mid-top edition maintains the iconic look you love while choice colours and crisp leather give it a distinct identity.",
+      images: [
+        "/images/sample-products/a-one.png",
+        "/images/sample-products/a-two.png",
+      ],
+      price: 90.99,
+      brand: "Jordan",
+      rating: 4.8,
+      numReviews: 10,
+      stock: 5,
+      isFeatured: true,
+      banner: "banner-one.png",
+    },
+  ],
+};
+
+export default sampleData;
