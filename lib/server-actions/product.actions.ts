6ca5e45fd18c5d469c567844bceaa43dc212b2dd
Harry Dhillon <hdhillon478@gmail.com>
2025-01-26 08:25:44 +0000

    feat: use constant for latest product limit in fetching products

--- lib/server-actions/product.actions.ts
+++ lib/server-actions/product.actions.ts
@@ -4,12 +4,14 @@ import { PrismaClient } from "@prisma/client";
 
 import { convertToJavaScriptObject } from "../utils";
 
+import { LATEST_PRODUCT_LIMIT } from "../constants";
+
 export async function getLatestProducts() {
   const prisma = new PrismaClient();
 
   // fetch 4 latest products
   const data = await prisma.product.findMany({
-    take: 4,
+    take: LATEST_PRODUCT_LIMIT,
     orderBy: { createdAt: "desc" },
   });
 
