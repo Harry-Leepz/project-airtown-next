9e68b1c74a3aa38ca63c63bff55b128f2ece1d0e
Harry Dhillon <hdhillon478@gmail.com>
2025-02-02 16:22:50 +0000

    feat: enhance LogIn page layout with Card component and metadata

--- app/(auth)/log-in/page.tsx
+++ app/(auth)/log-in/page.tsx
@@ -1,3 +1,29 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Log In",
+};
+
 export default function LogIn() {
-  return <div>log in page</div>;
+  return (
+    <div className='w-full max-w-md mx-auto'>
+      <Card>
+        <CardHeader className='space-y-4'>
+          <CardTitle>Log In</CardTitle>
+          <CardDescription>Log in to your account.</CardDescription>
+        </CardHeader>
+
+        <CardContent className='space-y-4'>
+          {/* Log In Form goes here */}
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
