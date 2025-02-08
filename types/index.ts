308a1771a057ae798fe4855d0580953e78ec7443
Harry Dhillon <hdhillon478@gmail.com>
2025-02-08 19:32:13 +0000

    feat: add types for shopping bag and shopping bag items

--- types/index.ts
+++ types/index.ts
@@ -1,9 +1,17 @@
 import { z } from "zod";
 
-import { insertProductSchema } from "@/lib/validators";
+import {
+  insertProductSchema,
+  insertShoppingBagSchema,
+  shoppingBagItemSchema,
+} from "@/lib/validators";
 
 export type Product = z.infer<typeof insertProductSchema> & {
   id: string;
   rating: string;
   createdAt: Date;
 };
+
+export type ShoppingBag = z.infer<typeof insertShoppingBagSchema>;
+
+export type ShoppingBagItem = z.infer<typeof shoppingBagItemSchema>;
