a471d04ce8f4e0dfd1245d5fb3d047b0c2f4460f
Harry Dhillon <hdhillon478@gmail.com>
2025-02-09 09:34:51 +0000

    feat: add shadcn toaster for output to app layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import "./globals.css";
 
 import { ThemeProvider } from "next-themes";
 
+import { Toaster } from "@/components/ui/toaster";
+
 import { APP_DESCRIPTION, APP_NAME, SERVER_URL } from "@/lib/constants";
 
 const inter = Inter({
@@ -31,6 +33,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
+          <Toaster />
         </ThemeProvider>
       </body>
     </html>
