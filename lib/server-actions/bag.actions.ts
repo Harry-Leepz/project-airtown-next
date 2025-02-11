512cb8dbcc5fe3b6667e5605fb994fe1f6941d6f
Harry Dhillon <hdhillon478@gmail.com>
2025-02-11 21:25:59 +0000

    feat: implement addItemToBag function with session bag ID handling and error formatting

--- lib/server-actions/bag.actions.ts
+++ lib/server-actions/bag.actions.ts
@@ -1,10 +1,37 @@
 "use server";
 
+import { cookies } from "next/headers";
+
+import { formatError } from "../utils";
+
 import { ShoppingBagItem } from "@/types";
+import { auth } from "@/auth";
 
 export async function addItemToBag(data: ShoppingBagItem) {
-  return {
-    success: false,
-    message: "Woops",
-  };
+  try {
+    // check for shopping bag cookie
+    const sessionBagId = (await cookies()).get("sessionBagId")?.value;
+
+    if (!sessionBagId) throw new Error("Bag Session Not found");
+
+    // session and user id
+    const session = await auth();
+    const userId = session?.user?.id ? session.user.id : undefined;
+
+    // FOR TESTING ONLY -- NEEDS TO BE REMOVED
+    console.log({
+      "session bag id": sessionBagId,
+      "user id ": userId,
+    });
+
+    return {
+      success: true,
+      message: "Item added to bag",
+    };
+  } catch (error) {
+    return {
+      success: false,
+      message: formatError(error),
+    };
+  }
 }
