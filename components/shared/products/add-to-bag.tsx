4e449b3f669498288dd9a69ee65a197883de3837
Harry Dhillon <hdhillon478@gmail.com>
2025-02-16 20:43:45 +0000

    feat: add item removal functionality in AddToBag component with quantity display

--- components/shared/products/add-to-bag.tsx
+++ components/shared/products/add-to-bag.tsx
@@ -6,17 +6,21 @@ import { useToast } from "@/hooks/use-toast";
 import { Button } from "@/components/ui/button";
 import { ToastAction } from "@/components/ui/toast";
 
-import { PlusIcon } from "lucide-react";
+import { PlusIcon, MinusIcon } from "lucide-react";
 
-import { addItemToBag } from "@/lib/server-actions/bag.actions";
+import {
+  addItemToBag,
+  removeItemfromBag,
+} from "@/lib/server-actions/bag.actions";
 
-import { ShoppingBagItem } from "@/types";
+import { ShoppingBagItem, ShoppingBag } from "@/types";
 
 type AddToBagProps = {
   item: ShoppingBagItem;
+  bag?: ShoppingBag;
 };
 
-export default function AddToBag({ item }: AddToBagProps) {
+export default function AddToBag({ item, bag }: AddToBagProps) {
   const router = useRouter();
   const { toast } = useToast();
 
@@ -45,8 +49,33 @@ export default function AddToBag({ item }: AddToBagProps) {
     });
   }
 
-  return (
+  async function handleRemoveFromBag() {
+    const response = await removeItemfromBag(item.productId);
+
+    toast({
+      variant: response.success ? "default" : "destructive",
+      description: response.message,
+    });
+
+    return;
+  }
+
+  const itemExistsInBag =
+    bag && bag.items.find((product) => product.productId === item.productId);
+
+  return itemExistsInBag ? (
+    <div>
+      <Button type='button' variant='outline' onClick={handleRemoveFromBag}>
+        <MinusIcon className='h-4 w-4' />
+      </Button>
+      <span className='px-2'>{itemExistsInBag.quantity}</span>
+      <Button type='button' variant='outline' onClick={handleAddtoBag}>
+        <PlusIcon className='h-4 w-4' />
+      </Button>
+    </div>
+  ) : (
     <Button className='w-full' type='button' onClick={handleAddtoBag}>
+      {" "}
       <PlusIcon /> Add to bag
     </Button>
   );
