eaf00d0963b9ac25676fe5c598f3122661d762bf
Harry Dhillon <hdhillon478@gmail.com>
2025-02-19 20:55:18 +0000

    feat: implement BagTable component to display items in the shopping bag with quantity and price

--- components/shared/shopping-bag/bag-table.tsx
+++ components/shared/shopping-bag/bag-table.tsx
@@ -10,11 +10,21 @@ import { useToast } from "@/hooks/use-toast";
 import { Loader, ArrowRight, PlusIcon, MinusIcon } from "lucide-react";
 
 import {
+  Table,
+  TableBody,
+  TableRow,
+  TableCell,
+  TableHead,
+  TableHeader,
+} from "@/components/ui/table";
+
+import {
   addItemToBag,
   removeItemfromBag,
 } from "@/lib/server-actions/bag.actions";
 
 import { ShoppingBag } from "@/types";
+import { Button } from "@/components/ui/button";
 
 type BagTableProps = {
   bag?: ShoppingBag;
@@ -37,7 +47,90 @@ export default function BagTable({ bag }: BagTableProps) {
         ))}
       {bag && bag.items.length > 0 && (
         <div className='grid md:grid-cols-4 md:gap-5'>
-          <div className='overflow-x-auto md:col-span-3'>Table</div>
+          <div className='overflow-x-auto md:col-span-3'>
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Item</TableHead>
+                  <TableHead className='text-center'>Quantity</TableHead>
+                  <TableHead className='text-right'>Price</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {bag.items.map((item) => (
+                  <TableRow key={item.slug}>
+                    <TableCell>
+                      <Link
+                        href={`/product/${item.slug}`}
+                        className='flex items-center'
+                      >
+                        <Image
+                          src={item.image}
+                          alt={item.name}
+                          width={50}
+                          height={50}
+                          className='rounded-md'
+                        />
+                        <span className='px-2'>{item.name}</span>
+                      </Link>
+                    </TableCell>
+                    <TableCell className='flex-center gap-2'>
+                      <Button
+                        disabled={isPending}
+                        variant='outline'
+                        type='button'
+                        onClick={() =>
+                          startTransition(async () => {
+                            const response = await removeItemfromBag(
+                              item.productId
+                            );
+
+                            if (!response.success) {
+                              toast({
+                                variant: "destructive",
+                                description: response.message,
+                              });
+                            }
+                          })
+                        }
+                      >
+                        {isPending ? (
+                          <Loader className='w-4 h-4 animate-spin' />
+                        ) : (
+                          <MinusIcon className='w-4 h-4' />
+                        )}
+                      </Button>
+                      <span>{item.quantity}</span>
+                      <Button
+                        disabled={isPending}
+                        variant='outline'
+                        type='button'
+                        onClick={() =>
+                          startTransition(async () => {
+                            const response = await addItemToBag(item);
+
+                            if (!response.success) {
+                              toast({
+                                variant: "destructive",
+                                description: response.message,
+                              });
+                            }
+                          })
+                        }
+                      >
+                        {isPending ? (
+                          <Loader className='w-4 h-4 animate-spin' />
+                        ) : (
+                          <PlusIcon className='w-4 h-4' />
+                        )}
+                      </Button>
+                    </TableCell>
+                    <TableCell className='text-right'>{`£${item.price}`}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </div>
         </div>
       )}
     </>
