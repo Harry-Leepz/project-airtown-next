29f92a509365486ed5bb74858f49a37bdd8c4c66
Harry Dhillon <hdhillon478@gmail.com>
2025-05-14 20:22:26 +0100

    fix: update import statement for useRouter to use next/navigation

--- components/shared/payment-method/payment-method-form.tsx
+++ components/shared/payment-method/payment-method-form.tsx
@@ -4,7 +4,7 @@ import { useTransition } from "react";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
 import { useToast } from "@/hooks/use-toast";
 
