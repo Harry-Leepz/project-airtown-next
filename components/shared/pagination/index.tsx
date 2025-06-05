919145b83fd799f07745b2a83b9ebd49c9cd377a
Harry Dhillon <hdhillon478@gmail.com>
2025-06-05 09:45:33 +0100

    feat: implement Pagination component with URL query handling

--- components/shared/pagination/index.tsx
+++ components/shared/pagination/index.tsx
@@ -2,6 +2,11 @@
 
 import { useRouter, useSearchParams } from "next/navigation";
 
+import { Button } from "@/components/ui/button";
+import { ArrowLeft, ArrowRight } from "lucide-react";
+
+import { formUrlQuery } from "@/lib/utils";
+
 type PaginationProps = {
   page: number | string;
   totalPages: number;
@@ -16,5 +21,38 @@ export default function Pagination({
   const router = useRouter();
   const searchParams = useSearchParams();
 
-  return <div>Pagination</div>;
+  function handleClick(direction: "next" | "previous") {
+    const pageValue =
+      direction === "next" ? Number(page) + 1 : Number(page) - 1;
+    const newUrl = formUrlQuery({
+      params: searchParams.toString(),
+      key: urlParamName || "page",
+      value: pageValue.toString(),
+    });
+
+    router.push(newUrl);
+  }
+
+  return (
+    <div className='flex gap-2'>
+      <Button
+        size='lg'
+        variant='outline'
+        disabled={Number(page) === 1}
+        className='w-28'
+        onClick={() => handleClick("previous")}
+      >
+        <ArrowLeft /> Previous
+      </Button>
+      <Button
+        size='lg'
+        variant='outline'
+        disabled={Number(page) >= totalPages}
+        className='w-28'
+        onClick={() => handleClick("next")}
+      >
+        Next <ArrowRight />
+      </Button>
+    </div>
+  );
 }
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,8 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+import qs from "query-string";
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
@@ -124,4 +126,28 @@ export function formatDateTime(dateString: Date) {
     dateOnly: formattedDate,
     timeOnly: formattedTime,
   };
+}
+
+// form pagination url
+export function formUrlQuery({
+  params,
+  key,
+  value,
+}: {
+  params: string;
+  key: string;
+  value: string | null;
+}) {
+  const query = qs.parse(params);
+  query[key] = value;
+
+  return qs.stringifyUrl(
+    {
+      url: window.location.pathname,
+      query: query,
+    },
+    {
+      skipNull: true,
+    }
+  );
 }
