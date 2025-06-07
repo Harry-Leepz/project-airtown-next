4e5923e74e21feafcdfaa019ae766c1d42d8937c
Harry Dhillon <hdhillon478@gmail.com>
2025-06-07 16:12:33 +0100

    feat: enhance user profile page with session handling and metadata

--- app/user/profile/page.tsx
+++ app/user/profile/page.tsx
@@ -1,5 +1,21 @@
-import React from "react";
+import { Metadata } from "next";
 
-export default function Profile() {
-  return <div>page</div>;
+import { SessionProvider } from "next-auth/react";
+import { auth } from "@/auth";
+
+export const metadata: Metadata = {
+  title: "User Profile",
+  description: "User profile page",
+};
+
+export default async function Profile() {
+  const session = await auth();
+  return (
+    <SessionProvider session={session}>
+      <div className='max-w-md mx-auto space-y-4'>
+        <h2 className='h2-bold'>Profile</h2>
+        {session?.user?.name}
+      </div>
+    </SessionProvider>
+  );
 }
