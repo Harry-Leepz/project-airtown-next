ae240f68dbb6f47620aef53e5c40652979913ac1
Harry Dhillon <hdhillon478@gmail.com>
2025-06-18 07:37:23 +0100

    feat: add AdminLayout component for admin page structure

---
+++ app/admin/layout.tsx
@@ -0,0 +1,41 @@
+import Link from "next/link";
+
+import Menu from "@/components/shared/header/menu";
+import AdminNav from "@/components/shared/admin-nav";
+
+import { APP_NAME } from "@/lib/constants";
+import { Input } from "@/components/ui/input";
+
+export default function AdminLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <div className='flex flex-col'>
+        <div className='container border-b mx-auto'>
+          <div className='flex items-center h-16 px-4'>
+            <Link href='/' className='w-22 '>
+              <span className='text-lg font-semibold'>{APP_NAME}</span>
+            </Link>
+            <AdminNav className='mx-6' />
+            <div className='ml-auto items-center flex space-x-4'>
+              <div>
+                <Input
+                  type='search'
+                  placeholder='Search...'
+                  className='md:w-[100px] lg:w-[300px]'
+                />
+              </div>
+              <Menu />
+            </div>
+          </div>
+        </div>
+        <div className='flex-1 space-y-4 p-8 pt-6 container mx-auto'>
+          {children}
+        </div>
+      </div>
+    </>
+  );
+}
