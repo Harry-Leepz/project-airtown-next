b6df53f51075079e34ae15f9994c6b89627f0233
Harry Dhillon <hdhillon478@gmail.com>
2025-08-10 06:46:59 +0100

    feat: update Admin Overview to include product count and format number display

--- app/admin/overview/page.tsx
+++ app/admin/overview/page.tsx
@@ -1,9 +1,17 @@
-import { auth } from "@/auth";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { getOrderSummary } from "@/lib/server-actions/order.actions";
-import { formatCurrency } from "@/lib/utils";
-import { BadgePoundSterling } from "lucide-react";
 import { Metadata } from "next";
+
+import { auth } from "@/auth";
+
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
+import { getOrderSummary } from "@/lib/server-actions/order.actions";
+import { formatCurrency, formatNumber } from "@/lib/utils";
+import {
+  BadgePoundSterling,
+  Barcode,
+  CreditCardIcon,
+  Users,
+} from "lucide-react";
 
 export const metadata: Metadata = {
   title: "Admin Dashboard",
@@ -23,9 +31,10 @@ export default async function AdminOverview() {
     <div className='space-y-2'>
       <h1 className='h2-bold'>Admin Dashboard</h1>
       <div className='grid gap-4 md:grid-cols-2 lg:grid-cols-4'>
+        {/* Sales */}
         <Card>
           <CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
-            <CardTitle className='text-sm font-medium'>Total Sales</CardTitle>
+            <CardTitle className='text-sm font-medium'>Total Revenue</CardTitle>
             <BadgePoundSterling />
           </CardHeader>
           <CardContent>
@@ -33,6 +42,42 @@ export default async function AdminOverview() {
               {formatCurrency(
                 summary.totalSales._sum.totalPrice?.toString() || 0
               )}
+            </div>
+          </CardContent>
+        </Card>
+        {/* Number of Orders Placed */}
+        <Card>
+          <CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
+            <CardTitle className='text-sm font-medium'>Sales</CardTitle>
+            <CreditCardIcon />
+          </CardHeader>
+          <CardContent>
+            <div className='text-2xl font-bold'>
+              {formatNumber(summary.ordersCount)}
+            </div>
+          </CardContent>
+        </Card>
+        {/* Number of Customers */}
+        <Card>
+          <CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
+            <CardTitle className='text-sm font-medium'>Customers</CardTitle>
+            <Users />
+          </CardHeader>
+          <CardContent>
+            <div className='text-2xl font-bold'>
+              {formatNumber(summary.usersCount)}
+            </div>
+          </CardContent>
+        </Card>
+        {/* Number of Products */}
+        <Card>
+          <CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
+            <CardTitle className='text-sm font-medium'>Products</CardTitle>
+            <Barcode />
+          </CardHeader>
+          <CardContent>
+            <div className='text-2xl font-bold'>
+              {formatNumber(summary.productsCount)}
             </div>
           </CardContent>
         </Card>
